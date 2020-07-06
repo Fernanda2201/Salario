97f585f94ea0de59b5ea1f11c90e858774300007
Fernanda2201 <67705246+Fernanda2201@users.noreply.github.com>
2020-07-06 17:35:01 -0500

    Add files via upload

---
+++ principal.h
@@ -0,0 +1,32 @@
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+#include <QMainWindow>
+#include "about.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Principal; }
+QT_END_NAMESPACE
+
+class Principal : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Principal(QWidget *parent = nullptr);
+    ~Principal();
+
+public slots:
+    void calcular();
+    void guardar();
+    void abrir();
+
+private slots:
+    void on_actionAcerca_de_triggered();
+
+private:
+    Ui::Principal *ui;
+    void limpiar();
+    About *about;
+};
+#endif // PRINCIPAL_H
